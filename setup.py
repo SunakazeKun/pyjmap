7a98f3cc6da1f3b40577d013af90d50edef62d9a
Aurum <nintendo.rpgforum@gmail.com>
2022-03-17 11:13:03 +0100

    First proper release, yay

--- setup.py
+++ setup.py
@@ -1,11 +1,16 @@
 import setuptools
 
+with open("README.md", "r") as f:
+    README = f.read()
+
 setuptools.setup(
     name="pyjmap",
-    version="1.0.0-beta",
+    version="1.0.0",
     author="Aurum",
     url="https://github.com/SunakazeKun/pyjmap",
     description="Python library for Nintendo's BCSV/JMap format",
+    long_description=README,
+    long_description_content_type="text/markdown",
     keywords=["nintendo", "jsystem", "jmap", "bcsv", "modding"],
     packages=setuptools.find_packages(),
     python_requires=">=3.6",
@@ -13,7 +18,7 @@ setuptools.setup(
     classifiers=[
         "Intended Audience :: Developers",
         "Topic :: Software Development :: Libraries",
-        "License :: OSI Approved :: GNU General Public License v3.0",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
         "Programming Language :: Python :: 3 :: Only"
     ],
     entry_points={
